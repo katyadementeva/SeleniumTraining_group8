74f81708b67aa21cc91af0d46226f191e4701e23
Ekaterina Dementyeva <katyadementeva@gmail.com>
2017-06-20 15:38:17 +0300

    TestProject for Задание 1. Подготовьте инфраструктуру

---
+++ TrainingProject1/Test1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace TrainingProject1
+{
+    [TestFixture]
+    public class Test1
+    {
+        IWebDriver driver;
+        WebDriverWait wait;
+
+        [SetUp]
+        public void StartTest()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        }
+
+        [Test]
+        public void TrainingTest1()
+        {
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(3);
+            driver.Navigate().GoToUrl("http://software-testing.ru/lms/login/index.php");
+            driver.FindElement(By.CssSelector("input[id=loginbtn]")).Click();
+            wait.Until(ExpectedConditions.ElementExists(By.CssSelector("span.error")));
+        }
+
+        [TearDown]
+        public void StopTest()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
